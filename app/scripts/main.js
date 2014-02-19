4293a681b18ec0927d1d307d70dcd3cbf9c3da52
Todd Heidenreich <jtheidenreich@gmail.com>
2014-02-19 17:57:49 -0500

    we be deleting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,12 +28,14 @@ $(document).ready(function(){
 
 $('.new-bear-btn').click(function(){
 
-	var bubba = {
-		name: $('.name').val(),
-		phone: $('.phone').val(),
-		bio: $('.bio').val(),
-		avatar: $('.image').val()
-	}
+	var bubba = new Contact()
+
+
+	if($('.name').val()) {bubba.set({name: $('.name').val()})}
+	if($('.phone').val()) {bubba.set({phone: $('.phone').val()})}
+	if($('.bio').val()) {bubba.set({bio: $('.bio').val()})}
+	if($('.avatar').val()) {bubba.set({avatar: $('.avatar').val()})}
+
 
 	// add the object literal into the collection.
 	// this will
@@ -48,5 +50,23 @@ $('.new-bear-btn').click(function(){
 
 	// Also, we can go ahead and .save() the new model to the server
 	freshModel.save()
+});
+
+
+
+window.deleteFunction = function(){
+contacts.each(function(dataStuff){
+console.log(dataStuff)
+$.ajax({
+type: 'delete',
+url: 'http://0.0.0.0:3000/collections/contacts/'+dataStuff.get("_id")
 })
+
+})
+}
+
+$('.delete-btn').click(function(){
+deleteFunction()
+})
+
 
